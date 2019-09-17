79ebd1fd45b5aed2a0e8bd96338e0f48cf9444c1
Viktar Rudzenia <viktarrudzenia@gmail.com>
2019-09-17 22:24:48 +0300

    add: 8kyu Multiply.js

---
+++ 8 kyu/Multiply.js
@@ -0,0 +1,11 @@
+/* DESCRIPTION
+    Create function which multiply one value to another
+*/
+
+function multiply(a, b) {
+  return a * b;
+}
+
+/* NOW With ES6 I solute this Kata as:
+   multiply = (a, b) => a * b;
+*/
