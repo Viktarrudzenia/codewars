1e743db69a11aa94720d430f8dfb58ea8ff455cf
Viktar Rudzenia <viktarrudzenia@gmail.com>
2019-09-18 08:28:38 +0300

    add: 6kyu Unique In Order

---
+++ 6 kyu/Unique In Order.js
@@ -0,0 +1,30 @@
+/* DESCRIPTION
+Implement the function unique_in_order which takes as argument a sequence and returns a list of items without any elements with the same value next to each other and preserving the original order of elements.
+
+For example:
+
+uniqueInOrder('AAAABBBCCDAABBB') == ['A', 'B', 'C', 'D', 'A', 'B']
+uniqueInOrder('ABBCcAD')         == ['A', 'B', 'C', 'c', 'A', 'D']
+uniqueInOrder([1,2,2,3,3])       == [1,2,3]
+*/
+
+/* TESTS
+Test.assertSimilar(uniqueInOrder('AAAABBBCCDAABBB'), ['A','B','C','D','A','B'])
+*/
+
+var uniqueInOrder = function(iterable) {
+  let result = [];
+  if (iterable.length == 0) {
+    return result;
+  }
+  if (typeof iterable == "string") {
+    iterable = iterable.split("");
+  }
+  result.push(iterable[0]);
+  for (let i = 0; i < iterable.length; i++) {
+    if (iterable[i] !== iterable[i + 1] && iterable[i + 1] !== undefined) {
+      result.push(iterable[i + 1]);
+    }
+  }
+  return result;
+};
