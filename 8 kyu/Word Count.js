8ee48d9827c4408aed65f4d266c7db349a38dcd8
Viktar Rudzenia <viktarrudzenia@gmail.com>
2019-10-22 10:16:25 +0300

    solve: 8kyu Word Count

---
+++ 8 kyu/Word Count.js
@@ -0,0 +1,55 @@
+/* DESCRIPTION
+Can you realize a function that returns word count from a given string?
+
+You have to ensure that spaces in string is a whitespace for real.
+
+What we want and finish of work:
+
+countWords("Hello"); // returns 1 as int
+countWords("Hello, World!") // returns 2
+countWords("No results for search term `s`") // returns 6
+countWords(" Hello") // returns 1
+// ... and so on
+What kind of tests we got for your code:
+
+Function have to count words, but not spaces, so be sure that it does right.
+Empty string has no words.
+String with spaces around should be trimmed.
+Non-whitespace (ex. breakspace, unicode chars) should be assumed as delimiter
+Be sure that words with chars like -, ', ` are counted right.
+*/
+
+/* TESTS
+describe("countWords", function() {
+  it("should work in basic form of problem", function() {
+    Test.assertEquals(countWords("Hello"), 1);
+    Test.assertEquals(countWords("Hello, World!"), 2);
+    Test.assertEquals(countWords("Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."), 19);
+    Test.assertEquals(countWords(""), 0);
+    Test.assertEquals(countWords("With! Symbol@ #Around! (Every) %Word$"), 5);
+    Test.assertEquals(countWords("Dear   Victoria, I love  to press   space button."), 8);
+  });
+  
+  it("should work with spaces around string", function() {
+    Test.assertEquals(countWords(" Arthur "), 1);
+    Test.assertEquals(countWords(" David"), 1);
+    Test.assertEquals(countWords("Nelson "), 1);
+    Test.assertEquals(countWords("  Hello Gomer  "), 2);
+    Test.assertEquals(countWords("  Hello     Bart  "), 2);
+  });
+  
+  it("should work with non-whitespace (ex. breakspace) chars", function() {
+    Test.assertEquals(countWords("﻿Hello﻿World "), 2);
+    Test.assertEquals(countWords("Hello﻿World"), 2);
+  });
+});
+*/
+
+function countWords(str) {
+  console.log(str);
+  let regExp = /[^A-Za-z0-9'-]+/g;
+  str = str.replace(regExp, " ").trim();
+  if (str.length === 0) return 0;
+  console.log(str);
+  return str.split(" ").length;
+}
